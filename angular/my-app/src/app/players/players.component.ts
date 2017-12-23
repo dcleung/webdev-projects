4385cb81811e4bd541b31905b4d03dfe5ac3bfae
dcleung <derex.leung@gmail.com>
2017-12-23 01:50:42 -0500

    Import angular structure

---
+++ angular/my-app/src/app/players/players.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../player';
+
+@Component({
+  selector: 'app-players',
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.css']
+})
+export class PlayersComponent implements OnInit {
+
+  player: Player = {
+    id: 1,
+    name: 'LeVeon Bell',
+    position: 'RB'
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
